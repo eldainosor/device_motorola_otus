0bf6b419c08b97187e56af369369ea0efef6d819
Shubham Singh <coolsks94@gmail.com>
2016-12-29 10:25:41 +0000

    AospExtended bringup

--- aosp_otus.mk
+++ aosp_otus.mk
@@ -1,8 +1,8 @@
 $(call inherit-product, device/motorola/otus/full_otus.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_otus
+PRODUCT_NAME := aosp_otus
 PRODUCT_RELEASE_NAME := MOTO E 3G
