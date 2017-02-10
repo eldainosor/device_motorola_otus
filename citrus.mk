df8695588a08699599ad99298b98c555f1a3d3ff
hamza-badar <hamzabadar05@gmail.com>
2017-02-10 12:24:33 +0000

    add some other shit

--- citrus.mk
+++ citrus.mk
@@ -3,6 +3,13 @@ $(call inherit-product, device/motorola/otus/citrus_otus.mk)
 # Inherit some common citrus stuff.
 $(call inherit-product, vendor/citrus/common.mk)
 
+# Must define platform variant before including any common things
+TARGET_BOARD_PLATFORM_VARIANT := msm8974
+
+# CAF REVISION
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.caf.branch=LA.BR.1.2.9_rb1.10
+
 # Maintainer
 PRODUCT_BUILD_PROP_OVERRIDES += DEVICE_MAINTAINERS="Hamza Badar"
 
