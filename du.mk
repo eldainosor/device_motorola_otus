ddf1d2108dd281883bed50e353cb9865f6fd9015
hamza-badar <hamzabadar05@gmail.com>
2017-02-15 15:23:06 +0000

    du-ify

--- du.mk
+++ du.mk
@@ -1,8 +1,8 @@
 $(call inherit-product, device/motorola/otus/full_otus.mk)
 
-# Inherit some common aosp stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common du stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_otus
+PRODUCT_NAME := du_otus
 PRODUCT_RELEASE_NAME := MOTO E 3G
