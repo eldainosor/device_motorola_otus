496d7b1892b38a071130ceef6c8d398aacc9b4d6
hamza-badar <hamzabadar05@gmail.com>
2017-02-20 10:58:21 +0000

    use magisk for root

--- aosp_otus.mk
+++ aosp_otus.mk
@@ -3,6 +3,9 @@ $(call inherit-product, device/motorola/otus/full_otus.mk)
 # Inherit some common aosp stuff.
 $(call inherit-product, vendor/aosp/common.mk)
 
+# Use Magisk
+DEFAULT_ROOT_METHOD := magisk
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := aosp_otus
 PRODUCT_RELEASE_NAME := MOTO E 3G
