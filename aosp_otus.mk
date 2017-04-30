607ec3158c2a1b48bdf5bf82208949584f065e12
hamza-badar <hamzabadar05@gmail.com>
2017-04-30 03:57:42 -0300

    fix stuff

--- aosp_otus.mk
+++ aosp_otus.mk
@@ -1,6 +1,11 @@
 $(call inherit-product, device/motorola/otus/device.mk)
 
+# Call vendor stuff
 $(call inherit-product-if-exists, vendor/motorola/surnia/otus-vendor.mk)
+
+# Bootanimation
+TARGET_SCREEN_WIDTH:= 540
+TARGET_SCREEN_HEIGHT:= 960
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := otus
